56ca2e516940f3665cd815cb732711db18a5d47c
awaxawa <adamwachsman@protonmail.com>
2025-10-21 01:26:55 +0000

    Made CharaterizedLine agnostic to specific font size values, WegItem gets unique font sizes from raw spans, added functionality to .get_characterized_lines

--- src/page.py
+++ src/page.py
@@ -51,30 +51,63 @@ def flatten_to_spans(page:dict):
 #     key:str
 #     filter_func:Callable # span[key] -> bool
 
+#     def to_func(self):
+#         def span_filter(span:Dict):
+#             val = span.get(self.key)
+#             if val is not None and self.filter_func(val):
+#                 return True
+#             else:
+#                 return False
+#         return span_filter
+
 class CharacterizedLine:
 
-    @staticmethod
-    def _is_title_span(span:Dict):
-        span_size = span.get('size', 0)
-        out = span_size >= 16.0
-        return out
+    # @staticmethod
+    # def _is_title_span(span:Dict):
+    #     span_size = span.get('size', 0)
+    #     out = span_size >= 16.0
+    #     return out
     
-    @staticmethod
-    def _is_section_heading_span(span:Dict):
-        span_size = span.get('size', 0)
-        out = (span_size >= 12.0) & (span_size < 16.0)
-        return out
+    # @staticmethod
+    # def _is_section_heading_span(span:Dict):
+    #     span_size = span.get('size', 0)
+    #     out = (span_size >= 12.0) & (span_size < 16.0)
+    #     return out
 
-    def __init__(self, raw_line:Dict):
+    def __init__(self, raw_line:Dict, font_sizes = [8.0, 12.0, 16.0]):
+        # Uses approximate logic based on contained spans to infer function of given lineS
+        assert len(font_sizes) >= 3, "Must have at least 3 font sizes to distinguish title, section, and body"
+        self.font_sizes = sorted(font_sizes)
         self._raw_line = raw_line
         self._raw_text = self.get_raw_text()
+        self._is_title_span, self._is_section_heading_span = self._build_span_funcs()
+        
         self.is_title_line = self._check_is_title()
         self.is_section_heading = self._check_is_section_heading()
         self.is_attribute_start = self._check_is_attribute_start()
         self._enforce_logical_precedence()
 
+    def _build_span_funcs(self):
+        
+        def _is_title_span(span:Dict) -> bool:
+            span_size = span.get('size', self.font_sizes[0])
+            return span_size >= self.font_sizes[-1]
+        
+        def _is_section_heading_span(span:Dict) -> bool:
+            span_size = span.get('size', self.font_sizes[0])
+            return (span_size > self.font_sizes[0]) & (span_size < self.font_sizes[-1])
+        
+        return _is_title_span, _is_section_heading_span
+
     def get_raw_text(self):
         return reduce(lambda x, y: x + y.get('text', ''), self._raw_line['spans'], '')
+
+    def get_key_val_split(self):
+        key_val_split = self._raw_text.split(': ', maxsplit=1)
+        if len(key_val_split) < 2:
+            raise ValueError(f'Error: Line was unable to be split based on ": "\nLine text: {self._raw_text}')
+        key, val = key_val_split
+        return key, val
 
     def _check_is_title(self):
         # Just check if it has largest font
@@ -108,34 +141,52 @@ class WegItem:
     CLIP = [0, 36, 800, 760]
     def __init__(self, doc:pymupdf.Document, start_index:int, stop_index:int = None):
         stop_index = stop_index if stop_index is not None else doc.page_count - 1
-        self.pages = doc.pages[start_index:stop_index]
+        self.pages = doc[start_index:stop_index]
+        self._all_extract_text = [p.get_textpage(clip = self.CLIP).extractDICT() for p in self.pages]
+        self._all_raw_blocks = reduce(lambda x, y: x + y, [p['blocks'] for p in self._all_extract_text])
+        self._all_raw_lines = reduce(generate_attr_flatten('lines'), self._all_raw_blocks, [])
+        self._all_raw_spans = reduce(generate_attr_flatten('spans'), self._all_raw_lines, [])
+        # Damn I didn't know list comprehension worked like this
+        self._font_sizes = tuple(set(s.get('size') for s in self._all_raw_spans))
+
+    def get_characterized_lines(self):
+        # Seemed like the easiest way to make sure font sizes gets passed each time
+        return tuple(map(CharacterizedLine, ((l, self._font_sizes) for l in self._all_raw_lines)))
 
 # t['blocks']
 # block['lines']
 if __name__ == "__main__":
     from pathlib import Path
 
-    docpath = Path(r'/workspaces/odin_reader/doc/fullwegexportcompressed.pdf')
+    docpath = Path(r'/workspaces/odin_reader/doc/tanksyourwelcome.pdf')
     doc = pymupdf.open(docpath)
 
-    for i in range(1):
-        p = doc.load_page(i)
-        t = p.get_text('dict', clip = [0, 36, 800, 760])
-        all_spans = flatten_to_spans(t)
+    item_indices = build_item_page_indices(doc)
+    index_pairs = [(item_indices[i], item_indices[i+1]) for i in range(len(item_indices) - 1)]
+    index_pairs.append((item_indices[-1], doc.page_count))
+    wegitems = []
+    for st, ed in index_pairs:
+        wegitems.append(WegItem(doc, st, ed))
 
-        sizes = set()
-        flags = set()
-        charflags = set()
-        colors = set()
-        fonts = set()
-        alphas = set()
-        for s in all_spans:
-            sizes.add(s.get('size', -1))
-            flags.add(s.get('flags', -1))
-            charflags.add(s.get('char_flags', -1))
-            colors.add(s.get('color', -1))
-            fonts.add(s.get('font', ''))
-            alphas.add(s.get('alpha', -1))
+    print()
+    # for i in range(1):
+        # p = doc.load_page(i)
+        # t = p.get_text('dict', clip = [0, 36, 800, 760])
+        # all_spans = flatten_to_spans(t)
+
+        # sizes = set()
+        # flags = set()
+        # charflags = set()
+        # colors = set()
+        # fonts = set()
+        # alphas = set()
+        # for s in all_spans:
+        #     sizes.add(s.get('size', -1))
+        #     flags.add(s.get('flags', -1))
+        #     charflags.add(s.get('char_flags', -1))
+        #     colors.add(s.get('color', -1))
+        #     fonts.add(s.get('font', ''))
+        #     alphas.add(s.get('alpha', -1))
 
         # debug_iter_print(sorted(sizes), 'Sizes')
         # debug_iter_print(sorted(flags), 'Flags')
@@ -144,48 +195,4 @@ if __name__ == "__main__":
         # debug_iter_print(sorted(fonts), 'Fonts')
         # debug_iter_print(sorted(alphas), 'Opacities')
 
-        pprint(tuple(filter(lambda x: x.get('size', 0.0) > 8.0, all_spans)))
-    # reader = pypdf.PdfReader(docpath)
-
-    # def test_visitor(text, user_matrix, tm_matrix, font_dictionary, font_size):
-    #     if font_dictionary is not None and not text.isspace() and font_size >= 12:
-    #         print("========")
-    #         print(text)
-    #         # print(user_matrix)
-    #         # print(tm_matrix)
-    #         print(font_dictionary)
-    #         print(font_size)
-    #         print("========")
-
-    # for i in range(10):
-    #     page = reader.get_page(i)
-    #     page.extract_text(visitor_text=test_visitor)
-
-
-#    {'alpha': 0,
-#   'ascender': 0.9000000357627869,
-#   'bbox': (10.75, 135.3000030517578, 42.28129959106445, 144.2519989013672),
-#   'bidi': 0,
-#   'char_flags': 0,
-#   'color': 0,
-#   'descender': -0.21900001168251038,
-#   'flags': 0,
-#   'font': 'Unnamed-T3',
-#   'origin': (10.75, 142.5),
-#   'size': 8.0,
-#   'text': 'Domain:'},
-#    {'alpha': 0,
-#   'ascender': 0.9000000357627869,
-#   'bbox': (44.5078010559082,
-#            135.3000030517578,
-#            56.491798400878906,
-#            144.24400329589844),
-#   'bidi': 0,
-#   'char_flags': 0,
-#   'color': 0,
-#   'descender': -0.21800000965595245,
-#   'flags': 0,
-#   'font': 'Unnamed-T3',
-#   'origin': (44.5078010559082, 142.5),
-#   'size': 8.0,
-#   'text': 'Air,'},
+        # pprint(tuple(filter(lambda x: x.get('size', 0.0) > 8.0, all_spans)))
